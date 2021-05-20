15e4caa8c3a41dc33a967f46f0988feb99679c45
Erick Santos <erickosantos.dev@gmail.com>
2021-05-20 15:05:12 -0300

    Exercício 2

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio2.js
@@ -0,0 +1,10 @@
+let num1 = 2;
+let num2 = 3;
+
+if (num1 > num2) {
+  console.log("O primeiro número é maior");
+} else if (num1 < num2) {
+  console.log("O segundo número é maior");
+} else {
+  console.log("Os números são iguais");
+}
