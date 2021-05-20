a7d60d561efb6b364623b3bcb682099bc20d7343
Erick Santos <erickosantos.dev@gmail.com>
2021-05-20 15:29:16 -0300

    Exercício 3

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio3.js
@@ -0,0 +1,11 @@
+let num1 = 5;
+let num2 = 7;
+let num3 = 4;
+
+if ((num1 > num2) && (num1 > num3)) {
+  console.log("O primeiro número é o maior");
+} else if ((num2 > num1) && (num2 > num3)) {
+  console.log("O segundo número é o maior");
+} else if ((num3 > num1) && (num3 > num2)) {
+  console.log("O terceiro número é o maior");
+}
