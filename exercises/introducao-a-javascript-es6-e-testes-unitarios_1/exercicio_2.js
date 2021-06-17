e94e880062b3ffce4a2369f24fc93acd0e13c5fe
Erick Santos <erickosantos.dev@gmail.com>
2021-06-17 18:47:16 -0300

    Exercício 2

---
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_1/exercicio_2.js
@@ -0,0 +1,4 @@
+let oddsAndEvens = [13, 3, 4, 10, 7, 2];
+oddsAndEvens = oddsAndEvens.sort((a,b)=>a-b).toString();
+
+console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`);
